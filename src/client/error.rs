68781361782b19d17b4f2ae8d2c2ce167b4fadc3
Łukasz Hanuszczak <hanuszczak@google.com>
2020-03-21 00:03:59 +0100

    Implement display traits for the client errors.

--- src/client/error.rs
+++ src/client/error.rs
@@ -3,15 +3,44 @@
 // Use of this source code is governed by an MIT-style license that can be found
 // in the LICENSE file or at https://opensource.org/licenses/MIT.
 
+use std::fmt::{Display, Formatter};
+
+#[derive(Debug)]
 pub enum ReadError {
     Input(std::io::Error),
     Decode(prost::DecodeError),
     Magic(u32),
 }
 
+#[derive(Debug)]
 pub enum WriteError {
     Output(std::io::Error),
     Encode(prost::EncodeError),
+}
+
+impl Display for ReadError {
+
+    fn fmt(&self, fmt: &mut Formatter) -> std::fmt::Result {
+        use ReadError::*;
+
+        match *self {
+            Input(ref err) => write!(fmt, "input error: {}", err),
+            Decode(ref err) => write!(fmt, "proto decoding error: {}", err),
+            Magic(magic) => write!(fmt, "invalid magic: {}", magic),
+        }
+    }
+}
+
+impl Display for WriteError {
+
+    fn fmt(&self, fmt: &mut Formatter) -> std::fmt::Result {
+        use WriteError::*;
+
+        match *self {
+            Output(ref err) => write!(fmt, "output error: {}", err),
+            Encode(ref err) => write!(fmt, "proto encoding error: {}", err),
+        }
+    }
 }
 
 impl From<std::io::Error> for ReadError {
