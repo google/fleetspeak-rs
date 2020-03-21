e25a304e2240a7ba5df57cecbafdb4a6836d7943
Łukasz Hanuszczak <hanuszczak@google.com>
2020-03-21 23:04:26 +0100

    Update the example to use the collection function.

--- examples/hello.rs
+++ examples/hello.rs
@@ -3,14 +3,19 @@
 // Use of this source code is governed by an MIT-style license that can be found
 // in the LICENSE file or at https://opensource.org/licenses/MIT.
 
+use std::time::Duration;
+
 use fleetspeak::client::Packet;
 
 fn main() -> std::io::Result<()> {
     fleetspeak::client::startup("0.0.1")?;
 
     loop {
-        let request = fleetspeak::client::receive::<String>()?.data;
-        let response = format!("Hello {}!", request);
+        let packet = fleetspeak::client::collect(Duration::from_secs(1))?;
+
+        let request: String = packet.data;
+        let response: String = format!("Hello {}!", request);
+
         fleetspeak::client::send(Packet {
             service: String::from("greeter"),
             kind: None,
