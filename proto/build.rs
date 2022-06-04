d7d1683b81ba2cbab482a497c4cbe19cb8e98895
Łukasz Hanuszczak <lukasz.hanuszczak@gmail.com>
2022-06-04 10:28:07 +0200

    Improve formatting in the build script.

--- proto/build.rs
+++ proto/build.rs
@@ -3,23 +3,13 @@
 // Use of this source code is governed by an MIT-style license that can be found
 // in the LICENSE file or at https://opensource.org/licenses/MIT.
 
-// extern crate prost_build;
 use std::io::Result;
 use std::path::PathBuf;
-
-const PROTOS: &'static [&'static str] = &[
-    "fleetspeak/fleetspeak/src/common/proto/fleetspeak/common.proto",
-    "fleetspeak/fleetspeak/src/client/channel/proto/fleetspeak_channel/channel.proto",
-];
-
-const INCLUDES: &'static [&'static str] = &[
-    "fleetspeak/fleetspeak/src",
-];
 
 fn cargo_out_dir() -> PathBuf {
     let out_dir = std::env::var("OUT_DIR")
         .expect("output folder not specified");
-    
+
     PathBuf::from(out_dir)
 }
 
@@ -33,8 +23,9 @@ fn main() -> Result<()> {
 
     protobuf_codegen_pure::Codegen::new()
         .out_dir(&proto_out_dir.to_str().unwrap())
-        .includes(INCLUDES)
-        .inputs(PROTOS)
+        .include("fleetspeak/fleetspeak/src")
+        .input("fleetspeak/fleetspeak/src/common/proto/fleetspeak/common.proto")
+        .input("fleetspeak/fleetspeak/src/client/channel/proto/fleetspeak_channel/channel.proto")
         .customize(protobuf_codegen_pure::Customize {
             gen_mod_rs: Some(true),
             ..Default::default()
