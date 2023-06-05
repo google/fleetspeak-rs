dd5fe72c53359ff3a8f77b0a96862212051a3f3f
Łukasz Hanuszczak <hanuszczak@google.com>
2023-06-05 10:55:34 +0200

    Define a custom type for magic errors.

--- src/error.rs
+++ src/error.rs
@@ -150,3 +150,27 @@ impl From<WriteError> for std::io::Error {
         }
     }
 }
+
+/// Invalid magic number was read from the input stream.
+#[derive(Debug)]
+pub(crate) struct InvalidMagicError {
+    /// Invalid magic that was read from the input stream.
+    pub(crate) magic: u32,
+}
+
+impl std::fmt::Display for InvalidMagicError {
+
+    fn fmt(&self, fmt: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(fmt, "invalid Fleetspeak magic: 0x{:08x}", self.magic)
+    }
+}
+
+impl std::error::Error for InvalidMagicError {
+}
+
+impl From<InvalidMagicError> for std::io::Error {
+
+    fn from(error: InvalidMagicError) -> std::io::Error {
+        std::io::Error::new(std::io::ErrorKind::InvalidData, error)
+    }
+}
